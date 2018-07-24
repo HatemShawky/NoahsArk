60c358bc358c19a631036be06c920789f587c3f1
HatemShawky <40733346+HatemShawky@users.noreply.github.com>
2018-07-24 21:13:31 +0200

    Update urls.py

--- ProjectNoahsArk/Pizza/urls.py
+++ ProjectNoahsArk/Pizza/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pizza/results/',views.resultspage, name="pizza_results_page"),
-    path('pizza/', views.inputpage, name='pizza_input_page'),
+    path('results/',views.resultspage, name="pizza_results_page"),
+    path('input/', views.inputpage, name='pizza_input_page'),
 ]
