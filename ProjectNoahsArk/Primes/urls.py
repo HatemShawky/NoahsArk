a86e1783b17c3bc18f380ba47c0699e10d3d7014
HatemShawky <40733346+HatemShawky@users.noreply.github.com>
2018-07-24 21:13:56 +0200

    Update urls.py

--- ProjectNoahsArk/Primes/urls.py
+++ ProjectNoahsArk/Primes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('primes/results/',views.resultspage, name="primes_results_page"),
-    path('primes/', views.inputpage, name='primes_input_page'),
+    path('results/',views.resultspage, name="primes_results_page"),
+    path('input/', views.inputpage, name='primes_input_page'),
 ]
